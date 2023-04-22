f21ad53b736aac089ce359b1e535a647e647afb7
Thomas Paul <thomaspauljs@outlook.com>
2023-04-22 08:56:41 -0500

    Following Java SE 17 Unit Testing with JUnit. Init Commit

---
+++ src/test/java/rxwriter/prescription/DurationParserTest.java
@@ -0,0 +1,13 @@
+package rxwriter.prescription;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DurationParserTest {
+
+    @Test
+    public void parseDurationWithValidUnitAndQuantity() {
+
+    }
+}
